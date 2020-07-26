d42a0b84e472cfdb9365ba75858e1706002cf532
figroc <figroc@gmail.com>
2020-07-26 13:54:15 +0800

    fix(pvarint): unit tests (#10)
    * feat(pvarint): unit tests
    * informative message on test failure

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/yomorun/yomo-codec-golang
 
 go 1.14
+
+require (
+	github.com/stretchr/testify v1.6.1
+)
--- pkg/pvarint_test.go
+++ pkg/pvarint_test.go
@@ -1,388 +1,107 @@
 package encoding
 
 import (
+	"fmt"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestU1Byte(t *testing.T) {
-	buf := []byte{0x01}
-	res, _, err := Upvarint(buf, 0)
-	var expected uint32 = 1
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 1 {
-	// 	t.Errorf("_ should %v, actual = %v", 1, _)
-	// }
+func TestInt32(t *testing.T) {
+	testPVarInt32(t, -1, []byte{0x7F})
+	testPVarInt32(t, -5, []byte{0x7B})
+	testPVarInt32(t, 63, []byte{0x3F})
+	testPVarInt32(t, -65, []byte{0xFF, 0x3F})
+	testPVarInt32(t, 127, []byte{0x80, 0x7F})
+	testPVarInt32(t, 255, []byte{0x81, 0x7F})
+	testPVarInt32(t, -4097, []byte{0xDF, 0x7F})
+	testPVarInt32(t, -8193, []byte{0xFF, 0xBF, 0x7F})
+	testPVarInt32(t, -2097152, []byte{0xFF, 0x80, 0x80, 0x00})
+	testPVarInt32(t, -134217729, []byte{0xFF, 0xBF, 0xFF, 0xFF, 0x7F})
+	testPVarInt32(t, -2147483648, []byte{0xF8, 0x80, 0x80, 0x80, 0x00})
 }
 
-func TestUWrongByte(t *testing.T) {
-	buf := []byte{0x81, 0x82}
-	_, _, err := Upvarint(buf, 0)
-	if err == nil {
-		t.Errorf("err should not nil")
-	}
-	if err.Error() != "buffer insufficient" {
-		t.Errorf("err should not nil, actual err = %v", err)
-	}
+func TestUInt32(t *testing.T) {
+	testPVarUInt32(t, 1, []byte{0x01})
+	testPVarUInt32(t, 127, []byte{0x80, 0x7F})
+	testPVarUInt32(t, 128, []byte{0x81, 0x00})
+	testPVarUInt32(t, 130, []byte{0x81, 0x02})
+	testPVarUInt32(t, 1048576, []byte{0x80, 0xC0, 0x80, 0x00})
+	testPVarUInt32(t, 134217728, []byte{0x80, 0xC0, 0x80, 0x80, 0x00})
+	testPVarUInt32(t, 4294967295, []byte{0x7F})
 }
 
-func TestU2Bytes(t *testing.T) {
-	buf := []byte{0x81, 0x02}
-	res, _, err := Upvarint(buf, 0)
-	var expected uint32 = 130
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 2 {
-	// 	t.Errorf("_ should %v, actual = %v", 2, _)
-	// }
+func TestInt64(t *testing.T) {
+	testPVarInt64(t, 0, []byte{0x00})
+	testPVarInt64(t, 1, []byte{0x01})
+	testPVarInt64(t, -1, []byte{0x7F})
 }
 
-func TestU2BytesSkip(t *testing.T) {
-	buf := []byte{0x81, 0x82, 0x81, 0x02}
-	res, _, err := Upvarint(buf, 2)
-	var expected uint32 = 130
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 2 {
-	// 	t.Errorf("_ should %v, actual = %v", 2, _)
-	// }
+func TestUInt64(t *testing.T) {
+	testPVarUInt64(t, 0, []byte{0x00})
+	testPVarUInt64(t, 1, []byte{0x01})
+	testPVarUInt64(t, 18446744073709551615, []byte{0x7F})
 }
 
-func Test1ByteNagitive(t *testing.T) {
-	buf := []byte{0x7B}
-	res, _, err := Pvarint(buf, 0)
-	var expected int32 = -5
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 1 {
-	// 	t.Errorf("_ should %v, actual = %v", 1, _)
-	// }
+func testPVarInt32(t *testing.T, value int32, bytes []byte) {
+	var msg = fmt.Sprintf("testing %v: %v", value, bytes)
+	var size = SizeOfPVarInt32(value)
+	assert.Equal(t, len(bytes), size, msg)
+
+	buffer := make([]byte, len(bytes))
+	codec := VarIntCodec{Size: size}
+	assert.Nil(t, codec.EncodePVarInt32(buffer, value), msg)
+	assert.Equal(t, bytes, buffer, msg)
+
+	var val int32
+	codec = VarIntCodec{}
+	assert.Nil(t, codec.DecodePVarInt32(bytes, &val), msg)
+	assert.Equal(t, value, val, msg)
 }
 
-func Test1BytePositive(t *testing.T) {
-	buf := []byte{0x3F}
-	res, _, err := Pvarint(buf, 0)
-	var expected int32 = 63
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 1 {
-	// 	t.Errorf("_ should %v, actual = %v", 1, _)
-	// }
+func testPVarUInt32(t *testing.T, value uint32, bytes []byte) {
+	var msg = fmt.Sprintf("testing %v: %v", value, bytes)
+	var size = SizeOfPVarUInt32(value)
+	assert.Equal(t, len(bytes), size, msg)
+
+	buffer := make([]byte, len(bytes))
+	codec := VarIntCodec{Size: size}
+	assert.Nil(t, codec.EncodePVarUInt32(buffer, value), msg)
+	assert.Equal(t, bytes, buffer, msg)
+
+	var val uint32
+	codec = VarIntCodec{}
+	assert.Nil(t, codec.DecodePVarUInt32(bytes, &val), msg)
+	assert.Equal(t, value, val, msg)
 }
 
-func Test2BytesNagitive(t *testing.T) {
-	buf := []byte{0xFF, 0x3F}
-	res, _, err := Pvarint(buf, 0)
-	var expected int32 = -65
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 2 {
-	// 	t.Errorf("_ should %v, actual = %v", 2, _)
-	// }
+func testPVarInt64(t *testing.T, value int64, bytes []byte) {
+	var msg = fmt.Sprintf("testing %v: %v", value, bytes)
+	var size = SizeOfPVarInt64(value)
+	assert.Equal(t, len(bytes), size, msg)
+
+	buffer := make([]byte, len(bytes))
+	codec := VarIntCodec{Size: size}
+	assert.Nil(t, codec.EncodePVarInt64(buffer, value), msg)
+	assert.Equal(t, bytes, buffer, msg)
+
+	var val int64
+	codec = VarIntCodec{}
+	assert.Nil(t, codec.DecodePVarInt64(bytes, &val), msg)
+	assert.Equal(t, value, val, msg)
 }
 
-func Test2BytesPositive2(t *testing.T) {
-	buf := []byte{0x80, 0x7F}
-	res, _, err := Pvarint(buf, 0)
-	var expected int32 = 127
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 2 {
-	// 	t.Errorf("_ should %v, actual = %v", 2, _)
-	// }
-}
+func testPVarUInt64(t *testing.T, value uint64, bytes []byte) {
+	var msg = fmt.Sprintf("testing %v: %v", value, bytes)
+	var size = SizeOfPVarUInt64(value)
+	assert.Equal(t, len(bytes), size, msg)
 
-func Test2BytesPositive(t *testing.T) {
-	buf := []byte{0xFF, 0x81, 0x7F}
-	res, _, err := Pvarint(buf, 1)
-	var expected int32 = 255
-	if err != nil {
-		t.Errorf("err should nil, actual = %v", err)
-	}
-	if res != expected {
-		t.Errorf("value should %v, actual = %v", expected, res)
-	}
-	// if _ != 2 {
-	// 	t.Errorf("_ should %v, actual = %v", 2, _)
-	// }
-}
+	buffer := make([]byte, len(bytes))
+	codec := VarIntCodec{Size: size}
+	assert.Nil(t, codec.EncodePVarUInt64(buffer, value), msg)
+	assert.Equal(t, bytes, buffer, msg)
 
-func TestUpvarintEncode1(t *testing.T) {
-	var val uint32 = 127
-	expected := []byte{0x7F}
-	res, length, err := EncodeUpvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != 1 {
-		t.Errorf("expected length=1, actual=%v", length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-	if res[0] != expected[0] {
-		t.Errorf("expected res=%#x, actual=%#x", expected, res)
-	}
-}
-
-func TestUpvarintEncode2(t *testing.T) {
-	var val uint32 = 128
-	expected := []byte{0x81, 0x00}
-	res, length, err := EncodeUpvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != 2 {
-		t.Errorf("expected length=2, actual=%v", length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-	if res[0] != expected[0] {
-		t.Errorf("expected res=%#x, actual=%#x", expected, res)
-	}
-}
-
-func TestUpvarintEncode3(t *testing.T) {
-	var val uint32 = 1048576
-	expected := []byte{0xC0, 0x80, 0x00}
-	res, length, err := EncodeUpvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != 3 {
-		t.Errorf("expected length=3, actual=%v", length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-	if res[0] != expected[0] {
-		t.Errorf("expected res=%#x, actual=%#x", expected, res)
-	}
-}
-
-func TestUpvarintEncode4(t *testing.T) {
-	var val uint32 = 134217728
-	expected := []byte{0xC0, 0x80, 0x80, 0x00}
-	res, length, err := EncodeUpvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != 4 {
-		t.Errorf("expected length=4, actual=%v", length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-	if res[0] != expected[0] {
-		t.Errorf("expected res=%#x, actual=%#x", expected, res)
-	}
-}
-
-func TestUpvarintEncode5(t *testing.T) {
-	var val uint32 = 4294967295
-	expected := []byte{0x8F, 0xFF, 0xFF, 0xFF, 0x07F}
-	res, length, err := EncodeUpvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != 5 {
-		t.Errorf("expected length=5, actual=%v", length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-	if res[0] != expected[0] {
-		t.Errorf("expected res=%#x, actual=%#x", expected, res)
-	}
-}
-
-func TestPvarintEncode1(t *testing.T) {
-	var val int32 = -1
-	expected := []byte{0x7F}
-	res, length, err := EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != 1 {
-		t.Errorf("expected length=1, actual=%v", length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-	if res[0] != expected[0] {
-		t.Errorf("expected res=%#x, actual=%#x", expected, res)
-	}
-
-	val = -64
-	expected = []byte{0x40}
-	res, length, err = EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != 1 {
-		t.Errorf("expected length=1, actual=%v", length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-	if res[0] != expected[0] {
-		t.Errorf("expected res=%#x, actual=%#x", expected, res)
-	}
-}
-
-func TestPvarintEncode11(t *testing.T) {
-	var val int32 = -65
-	expected := []byte{0xFF, 0x3F}
-	res, length, err := EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-
-	for i, v := range res {
-		if v != expected[i] {
-			t.Errorf("expected res=%#x, actual=%#x", expected[i], v)
-		}
-	}
-}
-
-func TestPvarintEncode2(t *testing.T) {
-	var val int32 = -4097
-	expected := []byte{0xDF, 0x7F}
-	res, length, err := EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-
-	for i, v := range res {
-		if v != expected[i] {
-			t.Errorf("expected res=%#x, actual=%#x", expected[i], v)
-		}
-	}
-}
-
-func TestPvarintEncode3(t *testing.T) {
-	var val int32 = -8193
-	expected := []byte{0xFF, 0xBF, 0x7F}
-	res, length, err := EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-
-	for i, v := range res {
-		if v != expected[i] {
-			t.Errorf("expected res=%#x, actual=%#x", expected[i], v)
-		}
-	}
-}
-
-func TestPvarintEncode4(t *testing.T) {
-	var val int32 = -2097152
-	expected := []byte{0xFF, 0x80, 0x80, 0x00}
-	res, length, err := EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-
-	for i, v := range res {
-		if v != expected[i] {
-			t.Errorf("expected res=%#x, actual=%#x", expected[i], v)
-		}
-	}
-}
-
-func TestPvarintEncode5(t *testing.T) {
-	var val int32 = -134217729
-	expected := []byte{0xFF, 0xBF, 0xFF, 0xFF, 0x7F}
-	// var vic = new(VarIntCodec)
-	// res := make([]byte, 10)
-	// err := vic.EncodeInt32(res, val)
-	res, _, err := EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-
-	for i, v := range res {
-		if v != expected[i] {
-			t.Errorf("expected res=%#x, actual=%#x", expected[i], v)
-		}
-	}
-}
-
-func TestPvarintEncode51(t *testing.T) {
-	var val int32 = -2147483648
-	expected := []byte{0xF8, 0x80, 0x80, 0x80, 0x00}
-	res, length, err := EncodePvarint(val)
-	if err != nil {
-		t.Errorf("expected err=nil, actual=%v", err)
-	}
-	if length != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), length)
-	}
-	if len(res) != len(expected) {
-		t.Errorf("expected length=%v, actual=%v", len(expected), len(res))
-	}
-
-	for i, v := range res {
-		if v != expected[i] {
-			t.Errorf("expected res=%#x, actual=%#x", expected[i], v)
-		}
-	}
+	var val uint64
+	codec = VarIntCodec{}
+	assert.Nil(t, codec.DecodePVarUInt64(bytes, &val), msg)
+	assert.Equal(t, value, val)
 }
