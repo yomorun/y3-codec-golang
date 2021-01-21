ce3a7d67861533c80d43ccca04407577b82ec0da
chenjunbiao <chenjunbiao@outlook.com>
2021-01-21 16:28:37 +0800

    feat(example) add code for the explainer example

---
+++ examples/example1/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+
+	"github.com/yomorun/y3-codec-golang"
+)
+
+type SourceData struct {
+	Name  string      `yomo:"0x10"`
+	Noise float32     `yomo:"0x11"`
+	Therm Thermometer `yomo:"0x12"`
+}
+
+type Thermometer struct {
+	Temperature float32 `yomo:"0x13"`
+	Humidity    float32 `yomo:"0x14"`
+}
+
+func main() {
+	input := SourceData{
+		Name:  "yomo",
+		Noise: float32(456),
+		Therm: Thermometer{Temperature: float32(30), Humidity: float32(40)},
+	}
+
+	// encode interface to []byte
+	codec := y3.NewCodec(0x20)
+	inputBuf, _ := codec.Marshal(input)
+	fmt.Printf("inputBuf=%#v\n", inputBuf)
+
+	// define callback function to process the data being observed
+	callback := func(v []byte) (interface{}, error) {
+		return y3.ToFloat32(v)
+	}
+
+	// create the Observable interface
+	source := y3.FromStream(bytes.NewReader(inputBuf))
+	// subscribe observed to value
+	consumer := source.Subscribe(0x11).OnObserve(callback)
+	// checking data after it has been processed
+	for c := range consumer {
+		fmt.Printf("observed value=%v, type=%v\n", c, reflect.ValueOf(c).Kind())
+	}
+}
